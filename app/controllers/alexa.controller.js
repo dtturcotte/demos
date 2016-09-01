bdeb0ef598a27208e00b80d1d07de850db3f71c3
Daniel Turcotte <dthusky@gmail.com>
2016-09-01 16:14:14 -0400

    master

--- app/controllers/alexa.controller.js
+++ app/controllers/alexa.controller.js
@@ -3,15 +3,12 @@ module.exports = {
 	getUser : function (req, res, next) {
 		var users = require('../../data/alexa_data/users.json');
 		var user = users[0];
-		console.log('ALEXA GET USER', JSON.stringify(user));
 		return res.send(JSON.stringify(user));
 	},
 
 	getUserOrders : function (req, res, next) {
 		var orders = require('../../data/alexa_data/orders.json');
 		var order = orders[0];
-		console.log('ALEXA GET USER ORDERS STRINGIFIED', JSON.stringify(order));
-		console.log('ALEXA GET USER ORDERS', order);
 		var stringify = JSON.stringify(order);
 		return res.send(stringify);
 	},
@@ -19,8 +16,6 @@ module.exports = {
 	getSupplierByLocation : function (req, res, next) {
 		var suppliers_locations = require('../../data/alexa_data/suppliers.json');
 		var suppliers = suppliers_locations[0];
-		console.log('ALEXA GET SUPPLIERS STRINGIFIED', JSON.stringify(suppliers));
-		console.log('ALEXA GET SUPPLIERS', suppliers);
 		var stringify = JSON.stringify(suppliers);
 		return res.send(stringify);
 	},
@@ -28,8 +23,6 @@ module.exports = {
 	getSpecialOffers : function (req, res, next) {
 		var special_offers = require('../../data/alexa_data/special_offers.json');
 		var special_offer = special_offers[0];
-		console.log('ALEXA GET SPECIAL OFFERS STRINGIFIED', JSON.stringify(special_offer));
-		console.log('ALEXA GET SPECIAL OFFERS', special_offer);
 		var stringify = JSON.stringify(special_offer);
 		return res.send(stringify);
 	},
