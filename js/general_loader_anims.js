242331caa6c3325dcec92c33abdd16a03ffa4d71
Daniel Turcotte <dthusky@gmail.com>
2016-09-09 00:24:50 -0400

    master- site is now responsive

--- js/general_loader_anims.js
+++ js/general_loader_anims.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
 
 	var $window = $(window);
 
+	var scrolledToTop = true;
+	var animating = false;
+
 	window.window_width = $window.width();
 
 	$('.nav-items').find('span').each(function () {
@@ -14,7 +17,21 @@ $(document).ready(function () {
 	});
 
 	$(window).resize(function () {
-		console.log($window.width());
 		window.window_width = $window.width();
+	});
+
+	$(window).on('scroll', function() {
+		var scrollTop = $(this).scrollTop();
+		
+		if (window.window_width < 767 && scrollTop > 0 && scrolledToTop && !animating) {
+			animating = true;
+			$('.nav-items').animate({top:0}, 250, function() {
+				scrolledToTop = false;
+				animating = false;
+			});
+		} else if (window.window_width < 767 && scrollTop === 0 && !scrolledToTop) {
+			$(".nav-items").css('top', 50 + "px");
+			scrolledToTop = true;
+		}
 	});
 });
